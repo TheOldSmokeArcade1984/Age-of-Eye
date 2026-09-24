e5693c80ac2a2c7b41d5a4e7764aa0804b101dc6
TheOldSmokeArcade1984 <gianmarcofrattina@gmail.com>
2026-09-24 13:38:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const CACHE_NAME = 'age-of-eye-v1.0';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/hub_system.html',
-    '/knights_tour.html',
-    '/infilt_protocol.html',
+    './',
+    './index.html',
+    './hub_system.html',
+    './knights_tour.html',
+    './infilt_protocol.html',
     '/null_sector.html',
-    '/core.html',
-    '/epilogue.html',
-    '/ageofeye-progress.js',
-    '/preview.jpg',
-    '/icona.jpg'
+    './core.html',
+    './epilogue.html',
+    './ageofeye-progress.js',
+    './preview.jpg',
+    './icona.jpg'
 ];
 
 self.addEventListener('install', (event) => {
